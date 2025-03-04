a686dada8121fb340a241f17b95b6462f516beae
“davidggonz” <davidgg0902@gmail.com>
2025-03-04 09:40:03 -0600

    Second exam finished

--- Exams/Exam_02/circular.cpp
+++ Exams/Exam_02/circular.cpp
@@ -1,42 +1,38 @@
-#include <mpi.h> //Entorno MPI
-#include <iostream> //Por defecto
+#include <iostream>
+#include <mpi.h>
 
-int main(int argc, char** argv) { 
-    	//Inicialización el entorno MPI
-	MPI_Init(&argc, &argv);
-    	//Se definen tanto el rango como el número total de procesos
-	int rank, size;
-    	MPI_Comm_rank(MPI_COMM_WORLD, &rank);  //Rango del proceso actual
-    	MPI_Comm_size(MPI_COMM_WORLD, &size);  //Número total de procesos
+int main() {
+	//Se definen tanto el rango como el número de procesos
+   	int size, rank;
+	//Se define el buffer
+    	int buffer;
+	
+    	int tag = 0;
+	//Se inicializa el entorno MPI
+   	MPI_Init(NULL, NULL);
 
-    	int buffer_send = rank;  //Cada proceso almacena su rango en el buffer de envío
-    	int buffer_recv;         //Inicialmente, cada proceso tiene un buffer en el cual almacena su rango
+   	MPI_Comm_size(MPI_COMM_WORLD, &size);
+    	MPI_Comm_rank(MPI_COMM_WORLD, &rank);
 
-    	// Definir los rangos de los procesos vecinos
-    	int next_rank = (rank + 1) % size;
-    	int prev_rank = (rank - 1 + size) % size;
-
-    	// Realizar la comunicación circular
+    	buffer = rank;  //Buffer = rango del proceso
+	MPI_Status stat; //Se asigna el status a 'stat'
+	//Se inicializa el for para la comunicación circular
     	for (int i = 0; i < size; ++i) {
-		//Parte c) {{
-        	// Enviar el mensaje al proceso siguiente
-		// e)Comunicación bloqueada
-        	MPI_Send(&buffer_send, 1, MPI_INT, next_rank, 0, MPI_COMM_WORLD);//Cada proceso p le envı́a un mensaje al proceso p + 1
-
-        	// Recibir el mensaje del proceso anterior
-		// e)Cominucación bloqueada
-	        MPI_Recv(&buffer_recv, 1, MPI_INT, prev_rank, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);//Recibe un mensaje del proceso p − 1
-		//Final de la parte c}}
-		//Parte d)
-	        // Actualizar el buffer de envío con el mensaje recibido
-	        buffer_send = buffer_recv;
-		//Parte f)	
-        	// Imprimir el mensaje recibido
-	        std::cout << "Proceso " << rank << " recibió el mensaje " << buffer_recv << " del proceso " << prev_rank << std::endl;
-	    }
-
-	    // Finalizar el entorno MPI
-	    MPI_Finalize();
-
-	    return 0;
+        	int send_to = (rank + 1) % size; //Envío p+1
+        	int recv_from = (rank - 1 + size) % size; //Recibimiento p-1
+		//Para el rango actual:
+        	if (rank == 0) {
+			//Se actualiza el buffer con recv_from
+            		MPI_Sendrecv_replace(&buffer, 1, MPI_INT, send_to, tag, recv_from, tag, MPI_COMM_WORLD, &stat);
+			//Proceso actual tiene buffer 'p-1'
+            		std::cout << "Iteración " << i << ": Proceso 0 tiene buffer: " << buffer << std::endl;
+        	} else {
+			//Manejo de excepciones
+           		MPI_Send(&buffer, 1, MPI_INT, send_to, tag, MPI_COMM_WORLD);
+            		MPI_Recv(&buffer, 1, MPI_INT, recv_from, tag, MPI_COMM_WORLD, &stat);
+        	}
+    	}
+	//Finaliza el entorno MPI
+    	MPI_Finalize();
+    	return 0;
 }
